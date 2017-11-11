65e953424cec0ee0a0edb76412bac910c96bfaa7
Emily Pillmore <emilyp@cakesolutions.net>
2017-11-11 16:27:28 -0600

    partial State.hs

--- src/Course/State.hs
+++ src/Course/State.hs
@@ -38,8 +38,7 @@ exec ::
   State s a
   -> s
   -> s
-exec =
-  error "todo: Course.State#exec"
+exec (State rs) = snd . rs
 
 -- | Run the `State` seeded with `s` and retrieve the resulting value.
 --
@@ -48,8 +47,7 @@ eval ::
   State s a
   -> s
   -> a
-eval =
-  error "todo: Course.State#eval"
+eval (State rs) = fst . rs
 
 -- | A `State` where the state also distributes into the produced value.
 --
@@ -57,8 +55,7 @@ eval =
 -- (0,0)
 get ::
   State s s
-get =
-  error "todo: Course.State#get"
+get = State (\s -> (s, s))
 
 -- | A `State` where the resulting state is seeded with the given value.
 --
@@ -67,8 +64,7 @@ get =
 put ::
   s
   -> State s ()
-put =
-  error "todo: Course.State#put"
+put = State . const . (,) ()
 
 -- | Implement the `Functor` instance for `State s`.
 --
@@ -79,8 +75,9 @@ instance Functor (State s) where
     (a -> b)
     -> State s a
     -> State s b
-  (<$>) =
-    error "todo: Course.State#(<$>)"
+  f <$> (State rs) =
+      State (\s -> let (a, t) = rs s in (f a, t))
+
 
 -- | Implement the `Applicative` instance for `State s`.
 --
@@ -97,14 +94,15 @@ instance Applicative (State s) where
   pure ::
     a
     -> State s a
-  pure =
-    error "todo: Course.State pure#instance (State s)"
+  pure = State . (,)
   (<*>) ::
     State s (a -> b)
     -> State s a
-    -> State s b 
-  (<*>) =
-    error "todo: Course.State (<*>)#instance (State s)"
+    -> State s b
+  (State f) <*> (State k) =
+    State $ \s -> let (g, t) = f s
+                      (u, v) = k t
+                  in (g u, v)
 
 -- | Implement the `Bind` instance for `State s`.
 --
@@ -118,8 +116,10 @@ instance Monad (State s) where
     (a -> State s b)
     -> State s a
     -> State s b
-  (=<<) =
-    error "todo: Course.State (=<<)#instance (State s)"
+  f =<< State k =
+      State (\s -> let (a, t) = k s in runState (f a) t)
+
+
 
 -- | Find the first element in a `List` that satisfies a given predicate.
 -- It is possible that no element is found, hence an `Optional` result.
